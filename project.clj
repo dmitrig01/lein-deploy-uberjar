ca0f54b105bbce0e088a760edd2980967e672ee0
Dmitri G <dmitrig01@gmail.com>
2015-06-01 18:19:04 -0700

    damn...

--- project.clj
+++ project.clj
@@ -3,12 +3,4 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :eval-in-leiningen true
-  :repositories [["snapshots" {:url "http://admin.branch.io:8081/nexus/content/repositories/snapshots"
-                               :sign-releases false
-                               :username "dev"
-                               :password "kindred2015"}]
-                 ["releases" {:url "http://admin.branch.io:8081/nexus/content/repositories/releases"
-                              :sign-releases false
-                              :username "dev"
-                              :password "kindred2015"}]])
+  :eval-in-leiningen true)
