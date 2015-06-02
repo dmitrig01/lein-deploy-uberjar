276ba30c7f03d6988f9eb7c3460f1656698ce33b
Dmitri G <dmitrig01@gmail.com>
2015-06-01 22:07:54 -0700

    try redefsing twice

--- src/leiningen/deploy_uberjar.clj
+++ src/leiningen/deploy_uberjar.clj
@@ -1,26 +1,10 @@
 (ns leiningen.deploy-uberjar
   "Build and deploy jar to remote repository."
   (:require [leiningen.uberjar :as uberjar]
-  	        [leiningen.pom :as pom]
+            [leiningen.jar :as jar]
             [leiningen.deploy :as deploy]))
 
-(defn files-for [project repo]
-  (let [signed? (deploy/sign-for-repo? repo)
-        ;; If pom is put in "target/", :auto-clean true will remove it if the
-        ;; jar is created afterwards. So make jar first, then pom.
-        artifacts {[:extension "jar"] (uberjar/uberjar project) [:extension "pom"] (pom/pom project)}
-        sig-opts (deploy/signing-opts project repo)]
-    (prn "HERE!!!!" artifacts)
-    (if (and signed? (not (.endsWith (:version project) "-SNAPSHOT")))
-      (reduce merge artifacts (map #(deploy/signature-for-artifact % sig-opts)
-                                   artifacts))
-      artifacts)))
-
-(defn ^:no-project-needed deploy-uberjar
-  ([project]
-     (deploy-uberjar project (if (pom/snapshot? project)
-                       "snapshots"
-                       "releases")))
-  ([project repository]
-     (with-redefs [deploy/files-for files-for]
-       (deploy/deploy project repository))))
+(let [orig-jar jar/jar]
+  (defn ^:no-project-needed deploy-uberjar [project repository]
+    (with-redefs [jar/jar (fn [& args] (with-redefs [jar/jar orig-jar] {[:extension "jar"] (apply uberjar/uberjar args)}))]
+      (deploy/deploy project repository))))
